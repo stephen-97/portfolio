dc4a53a99e44d3c2ccc67907a540876d0e5eb315
stephen-97 <stephen.loiola@hotmail.com>
2023-06-27 13:23:06 +0200

    Initialisation et ajout du texte de description

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { CSSProperties} from 'react';
+import CSS from 'csstype'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={styles}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum, urna vitae aliquam venenatis, nunc arcu hendrerit felis, a fermentum nulla felis eget nibh. Pellentesque non lacus ornare, mattis leo et, dignissim erat. Sed vel metus purus. Maecenas mattis tellus sit amet nibh ornare, ac posuere odio rutrum.</div>
     </div>
   );
 }
+
+const styles: CSS.Properties = {
+    backgroundColor: "#cbded9",
+};
 
 export default App;
